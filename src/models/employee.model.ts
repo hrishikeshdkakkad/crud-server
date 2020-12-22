2faee4ea8167c5ad943e39ac284f910342a4f18f
Hrishikesh D Kakkad <hrishidkakkad@gmail.com>
2020-12-22 20:10:23 +0530

    Added inc count

--- src/models/employee.model.ts
+++ src/models/employee.model.ts
@@ -1,13 +1,19 @@
+import mongoose from 'mongoose';
 import { model, Schema, Document } from 'mongoose';
 
 import { IEmployee } from '../interfaces/employee.interface';
 
+const CounterSchema = Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 },
+});
+const counter = mongoose.model('counter', CounterSchema);
+
 export const employeeSchema: Schema<IEmployee> = new Schema({
   employeeID: {
     type: Number,
-    required: true,
     unique: true,
-    trim: true,
+    // default: 0,
   },
   name: {
     type: String,
@@ -31,6 +37,20 @@ export const employeeSchema: Schema<IEmployee> = new Schema({
   photo: {
     type: Buffer,
   },
+});
+
+employeeSchema.pre('save', function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const doc = this;
+  counter.findByIdAndUpdate({ _id: 'entitiyID' }, { $inc: { seq: 1 } }, { new: true, upsert: true }, function (error, counter): any {
+    if (error) {
+      return next(error);
+    }
+    console.log(counter.seq, 'sequence');
+    doc.employeeID = counter.seq;
+    console.log(this, 'this');
+    next();
+  });
 });
 
 const EmployeeModel = model<IEmployee & Document>('employee', employeeSchema);
